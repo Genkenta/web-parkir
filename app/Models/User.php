f9fb7f26e321ed5265ac2cbbd8cf319a37e79ec3
Genkenta <gentapriyakencana@gmail.com>
2026-02-23 09:12:34 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,20 +1,15 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName; // tambahkan ini
+use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasName // tambahkan HasName
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
     protected $table = 'tb_user';
     protected $primaryKey = 'id_user';
-    public $timestamps = false;
-
     protected $fillable = [
         'nama_lengkap',
         'username',
@@ -22,8 +17,22 @@ class User extends Authenticatable
         'role',
         'status_aktif',
     ];
-
     protected $hidden = [
         'password',
     ];
+
+    public function getAuthIdentifierName()
+    {
+        return 'username';
+    }
+
+    public function getFilamentName(): string
+    {
+        return (string) ($this->username ?? $this->nama_lengkap ?? 'User');
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
